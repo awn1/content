9471cca7a64f14cea9c4adeab4954dc856bb92f3
esharf <57587340+esharf@users.noreply.github.com>
2023-01-12 22:22:13 +0200

    added image family

--- server-image-build/run.sh
+++ server-image-build/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 time docker run -t --rm \
-    --env CI=$CI --env XSOAR_ADMIN_PASS=$XSOAR_ADMIN_PASS --env BASE_INSTANCE_NAME=$BASE_INSTANCE_NAME --env INSTALLER_URL=$INSTALLER_URL --env SERVER_IMG_NAME=$SERVER_IMG_NAME \
+    --env CI=$CI --env XSOAR_ADMIN_PASS=$XSOAR_ADMIN_PASS --env BASE_INSTANCE_NAME=$BASE_INSTANCE_NAME --env INSTALLER_URL=$INSTALLER_URL --env SERVER_IMG_NAME=$SERVER_IMG_NAME --env VERSION=$VERSION \
     --mount type=bind,source=$(pwd)/server-image-build,target=/server-image-build \
     --mount "type=bind,source=$XSOAR_CONTENT_BUILD_CREDS,target=/server-image-build/secrets/xsoar_content_build_creds" \
     --mount "type=bind,source=$XSOAR_SERVER_STORAGE_CREDS,target=/server-image-build/secrets/xsoar_server_storage_creds" \
