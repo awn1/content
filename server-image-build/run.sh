0e95110784d6bf52eff42b608409b8d392847810
esharf <57587340+esharf@users.noreply.github.com>
2023-01-17 20:12:21 +0200

    fixup! fixup! added content-test-data

--- server-image-build/run.sh
+++ server-image-build/run.sh
@@ -2,7 +2,7 @@
 time docker run -t --rm \
     --env CI=$CI --env XSOAR_ADMIN_PASS=$XSOAR_ADMIN_PASS --env BASE_INSTANCE_NAME=$BASE_INSTANCE_NAME \
     --env INSTALLER_URL=$INSTALLER_URL --env SERVER_IMG_NAME=$SERVER_IMG_NAME --env VERSION=$VERSION \
-    --env CI_PIPELINE_ID=$CI_PIPELINE_ID \
+    --env CI_PIPELINE_ID=$CI_PIPELINE_ID --env GITLAB_CONTENT_TEST_DATA_TOKEN=$GITLAB_CONTENT_TEST_DATA_TOKEN \
     --mount type=bind,source=$(pwd)/server-image-build,target=/server-image-build \
     --mount "type=bind,source=$XSOAR_CONTENT_BUILD_CREDS,target=/server-image-build/secrets/xsoar_content_build_creds" \
     --mount "type=bind,source=$XSOAR_SERVER_STORAGE_CREDS,target=/server-image-build/secrets/xsoar_server_storage_creds" \
