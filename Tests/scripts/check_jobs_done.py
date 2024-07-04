07d03210d2064b5d29578eebbf6ecdd90eeb5dff
Koby Meir <kmeir@paloaltonetworks.com>
2024-07-04 09:18:32 +0000

    The jobs-done-check-on-push step in the pipeline is missing some steps

--- Tests/scripts/check_jobs_done.py
+++ Tests/scripts/check_jobs_done.py
@@ -9,6 +9,7 @@ from Tests.scripts.utils.log_util import install_logging
 CONTENT_NIGHTLY_JOBS = [
     'run-pre-commit',
     'run-validations',
+    'run-validations-new-validate-flow',
     'mpv2-prepare-testing-bucket',
     'xpanse-prepare-testing-bucket',
     'xsoar-prepare-testing-bucket',
@@ -27,16 +28,18 @@ CONTENT_NIGHTLY_JOBS = [
 ]
 
 SDK_NIGHTLY_JOBS = [
-    'demisto-sdk-nightly:run-pre-commit',
-    'demisto-sdk-nightly:run-validations',
-    'demisto-sdk-nightly:run-validations-new-validate-flow',
-    'demisto_sdk_nightly:check_idset_dependent_commands',
-    'demisto-sdk-nightly:xsoar-prepare-testing-bucket',
-    'demisto-sdk-nightly:marketplacev2-prepare-testing-bucket',
-    'demisto-sdk-nightly:xpanse-prepare-testing-bucket',
-    'demisto-sdk-nightly:test-upload-flow',
-    'demisto-sdk-nightly:run-commands-against-instance',
-    'demisto-sdk-nightly:run-end-to-end-tests',
+     "demisto-sdk-nightly:run-pre-commit",
+     "demisto-sdk-nightly:run-validations",
+     "demisto-sdk-nightly:run-validations-new-validate-flow",
+     "demisto-sdk-nightly:test-infrastructure",
+     "demisto-sdk-nightly:test-upload-flow",
+     "demisto-sdk-nightly:check-idset-dependent-commands",
+     "demisto-sdk-nightly:mpv2-prepare-testing-bucket",
+     "demisto-sdk-nightly:xsoar-prepare-testing-bucket",
+     "demisto-sdk-nightly:xpanse-prepare-testing-bucket",
+     "demisto-sdk-nightly:xsoar-saas-prepare-testing-bucket",
+     "demisto-sdk-nightly:run-end-to-end-tests-general",
+     "demisto-sdk-nightly:run-end-to-end-tests-xsoar",
 ]
 
 BUCKET_UPLOAD_JOBS = [
