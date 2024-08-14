fe723ebc40851b5b2ff5746c69eb587f80399559
Dor Schwartz <dschwartz@paloaltonetworks.com>
2024-08-14 14:57:26 +0000

    Infra versioning: simplified process, README.md

---
+++ .gitlab/ci/ci_resources/infra-release-ci/README.md
@@ -0,0 +1,17 @@
+# Content infrastructure versioning
+
+## How to release an Infra version
+1.  Get a commit hash you want to release.
+    Make sure nightly has already passed on the commit hash! Otherwise, the release may break.
+2.  Run `poetry run .gitlab/ci/ci_resources/infra-release-ci/release_infra.py` with the necessary arguments. _(add ` --help` for more information)_
+3.  A new branch `infra_v0.0.0` (matching your version) will be pushed & merge request will be created. Go to the infra repo _merge requests_ section, and ask a TL/manager to approve it.
+4.  Merge to master. Check  https://gitlab.xdr.pan.local/xdr/cortex-content/infra/-/commits/master/?ref_type=HEADS. You'll see two commits under your name. Copy the commit sha of the earlier one (NOT the `Merge branch...` one).
+5.  Go to https://gitlab.xdr.pan.local/xdr/cortex-content/infra/-/releases/new. Type in a new tag `v.0.0.0` and then the commit hash you copied in the previous step (don't use the default `master`). Choose Release title=`Infra v0.0.0`. Copy the top of `CHANGELOG.md` (the changes included in your release) to the `Description` field. Click `Create Release`.
+6. Ask a GitLab maintainer (manager/principal eng.) to update the `INFRA_BRANCH` to the `v0.0.0` you just released, and Voilà!
+
+## Hotfixing
+Hotfixing is the process of creating a release with a temporary fix, that isn't intended to be merged.
+
+Do the same as above, but:
+- In step 4, do not merge to master.
+- In step 5, just use the commit sha that was printed at the end of step 2. (The commit that was automatically created with the release branch )
--- .gitlab/ci/ci_resources/infra-release-ci/release_infra.py
+++ .gitlab/ci/ci_resources/infra-release-ci/release_infra.py
@@ -1,88 +1,62 @@
 import itertools
-import os
 import subprocess
-import time
 from pathlib import Path
-from pprint import pprint
-from tempfile import NamedTemporaryFile
 
 import git
-import gitlab
 import typer
 import urllib3
 from dotenv import load_dotenv
-from gitlab.v4.objects.projects import Project
-from more_itertools import one
 from packaging.version import Version
-from tqdm import tqdm
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 load_dotenv(override=True)
-GITLAB_BASE_URL = "https://gitlab.xdr.pan.local"
 GITLAB_BASE_BRANCH = "master"
-GITLAB_GIT_URL = f"{GITLAB_BASE_URL.removeprefix('https://')}/xdr/cortex-content/infra.git"
-DEFAULT_PROJECT_ID = "1701"
 
-POLL_SLEEP_DURATION_S = 10
-POLL_MAX_SLEEP_S = 120
+GITLAB_BASE_URL = "https://gitlab.xdr.pan.local"
+GITLAB_INFRA_URL = f"{GITLAB_BASE_URL}/xdr/cortex-content/infra"
+GITLAB_GIT_URL = f"{GITLAB_INFRA_URL.removeprefix('https://')}.git"
+GITLAB_INFRA_PROJECT_ID = "1701"
 
+REMOTE_NAME = "origin"
 
 CHANGELOG_FRAHMENTS_FOLDER = Path(".changelogs")
 CHANGELOG_PATH = Path("CHANGELOG.md")
 PYPROJECT_PATH = Path("pyproject.toml")
 
-TMP_RELEASE_NOTES_PATH = Path(NamedTemporaryFile("w+").name)
-MERGE_REQUEST_LABELS = "infra_release"
+MERGE_REQUEST_LABELS = "Infra Release"
 
 app = typer.Typer(
-    pretty_exceptions_show_locals=False,  # prevent printing sensitive values on error
+    pretty_exceptions_show_locals=False  # prevent printing sensitive values on error
 )
 
 
 repo = git.Repo()
 
 
-def login_gitlab(project_id: str, gitlab_token: str) -> Project:
-    return gitlab.Gitlab(
-        url=GITLAB_BASE_URL,
-        private_token=gitlab_token,
-        ssl_verify=bool(os.getenv("CI")),
-    ).projects.get(id=project_id)
+def check_branch_doesnt_exist(repo: git.Repo, branch_name: str):
+    remote = repo.git.remote(REMOTE_NAME)
+    remote.fetch()
+
+    if branch_name in {ref.name for ref in remote.refs}:
+        raise ValueError(f"Branch {branch_name} already exists in remote, delete it from there.")
 
 
-def create_release_branch(
-    project: Project,
-    commit_to_release: str,
-    version: Version,
-    assignee: str,
-    gitlab_push_token: str,
-) -> str:
+def create_release_branch(commit_to_release: str, version: Version) -> None:
     """
     Bumps the version, creates release notes, and commits to a branch from the given commit.
     """
     branch_name = f"infra_release_v{version}"
-    title = branch_name.replace("_", " ").title().replace(" V", " v")
+    title = branch_name.replace("_", " ").title()
 
-    if project.branches.list(search=branch_name):
-        raise ValueError(f"Branch {branch_name} already exists in remote, delete it.")
-
-    repo.git.checkout("-b", branch_name, commit_to_release)  # new branch from commit_to_release
+    repo.git.checkout("-b", branch_name, commit_to_release)
 
     for command_parts in (
         ("poetry", "version", str(version)),  # Update version in pyptoject.toml
         ("towncrier", "build", "--version", str(version), "--yes"),  # Update Changelog
     ):
-        print("running", (command_str := " ".join(command_parts)))
+        print("running", (" ".join(command_parts)))
         try:
-            print(
-                subprocess.run(
-                    command_parts,
-                    text=True,
-                    check=True,
-                    capture_output=True,
-                ).stdout
-            )
-            print("Done", command_str)
+            print(subprocess.run(command_parts, text=True, check=True, capture_output=True).stdout)
 
         except subprocess.CalledProcessError as e:
             print(f"{e.returncode=}")
@@ -97,78 +71,40 @@ def create_release_branch(
         f"{CHANGELOG_FRAHMENTS_FOLDER}/*",  # remove old fragments
     )
     repo.git.commit("-m", title)
-    push_options = itertools.chain.from_iterable(
-        ("-o", option)
-        for option in (
-            "ci.skip",
-            "merge_request.create",
-            f"merge_request.target={GITLAB_BASE_BRANCH}",
-            f"merge_request.title={title}",
-            f"merge_request.label={MERGE_REQUEST_LABELS}",
-            f"merge_request.assign={assignee}",
+
+    repo.git.push(
+        *itertools.chain.from_iterable(
+            ("-o", option)
+            for option in (
+                "ci.skip",
+                "merge_request.create",
+                f"merge_request.target={GITLAB_BASE_BRANCH}",
+                f"merge_request.title={title}",
+                f"merge_request.label={MERGE_REQUEST_LABELS}",
+            )
         )
     )
-    repo.git.push(
-        "--set-upstream",
-        f"https://gitlab-ci-token:{gitlab_push_token}@{GITLAB_GIT_URL}",
-        *push_options,
-    )
+
     commit_sha = subprocess.run(("git", "rev-parse", "HEAD"), capture_output=True, text=True).stdout
-    print(f"pushed {commit_sha} successfully to new branch {branch_name}")
-    return commit_sha
 
-
-def create_gitlab_release(project: Project, release_commit: str, version: Version):
-    """Creates a GitLab Release for a commit hash"""
-    if not (mrs := project.mergerequests.list(state="opened", labels=MERGE_REQUEST_LABELS)):
-        raise ValueError(f"Cannot find an open MR with labels={MERGE_REQUEST_LABELS}")
-
-    mr = one(
-        mrs,
-        too_short=IndexError(f"Cannot find an open MR with the {MERGE_REQUEST_LABELS} label"),
-        too_long=IndexError(f"Found {len(mrs)}>1 MRs with the {MERGE_REQUEST_LABELS} label"),
+    print(
+        f"Pushed a new branch {branch_name}, commit sha={commit_sha}.\n"
+        f"Visit {GITLAB_INFRA_URL}/-/merge_requests to see the MR.\n"
+        f"After merging it, create a release in {GITLAB_INFRA_URL}/-/releases."
     )
-    # Poll until approved
-    mr_id = mr.attributes["iid"]  # iid is not a typo
-
-    print(f"MR ID: {mr_id}")
-    print(f"MR URL: {mr.attributes['web_url']}")
-
-    for _ in tqdm(tuple(range(POLL_MAX_SLEEP_S // POLL_SLEEP_DURATION_S))):
-        pprint(mr.asdict())  # TODO remove
-        if (state := mr.attributes.get("approval_state")) != "approved":
-            print(
-                f"MR #{mr_id} has {state=}"  # type:ignore[union-attr]
-                f"sleeping {POLL_SLEEP_DURATION_S}s hoping it's `approved` by the next iteration"
-            )
-            time.sleep(POLL_SLEEP_DURATION_S)
-
-    project.releases.create(
-        {
-            "name": f"Release v{version}",
-            "ref": release_commit,
-            "tag_name": f"v{version}",
-            "description": TMP_RELEASE_NOTES_PATH.read_text(),
-        }
-    )
-    print("Created release succesfully!")
 
 
 @app.command("release")
 def release(
-    gitlab_project_id: str = typer.Option(DEFAULT_PROJECT_ID, envvar="CI_PROJECT_ID"),
-    gitlab_token: str = typer.Option(envvar="GITLAB_CONTENT_CANCEL_TOKEN"),
-    start_commit: str = typer.Option(),
-    version: Version = typer.Option(..., "-v", envvar="RELEASE_VERSION", parser=Version),
-    assignee: str = typer.Option(envvar="RELEASE_MR_ASSIGNEE"),
-    gitlab_push_token: str = typer.Option(envvar="GITLAB_PUSH_TOKEN"),
+    start_commit: str = typer.Option(
+        "The commit hash to start the release from. IMPORTANT: Make sure it passed nightly before running!"
+    ),
+    version: Version = typer.Option("The version to release, e.g. 1.2.3", "-v", parser=Version),
 ):
     previous_branch = repo.active_branch.name
 
     try:
-        project = login_gitlab(gitlab_project_id, gitlab_token)
-        release_commit = create_release_branch(project, start_commit, version, assignee, gitlab_push_token)
-        create_gitlab_release(project, release_commit, version)
+        create_release_branch(start_commit, version)
 
     finally:
         print(f"Checking out back to {previous_branch}")
