3e7efd449bc9119783cba31b83f1c9e1d120cbd6
Michal Dagan <mdagan@paloaltonetworks.com>
2025-01-05 12:14:05 +0000

    Update triggers files

--- Utils/gitlab_triggers/trigger_upload_flow_build.sh
+++ Utils/gitlab_triggers/trigger_upload_flow_build.sh
@@ -1,21 +1,30 @@
 #!/usr/bin/env bash
+# This script triggers Content Upload in gitlab-CI.
+
+# For this script to work you will need to use a trigger token (see here for more about that: https://docs.gitlab.com/ee/ci/triggers/#create-a-pipeline-trigger-token)
+
+# This script requires the gitlab-ci trigger token. The branch to run against is an optional second parameter (the default is the current branch). The Slack channel to send messages to is an optional third parameter (the default is the 'dmst-bucket-upload')
+
+# Ways to run this script are:
+# trigger_upload_flow_build.sh -ct <trigger-token> [-b <content-branch-name> -ch <slack-channel-name>]
 
 if [ "$#" -lt "1" ]; then
   echo "Usage:
   $0 -ct <token>
 
-  -ct, --ci-token             The ci gitlab token.
-  [-b, --branch]              The branch name. Default is the current branch.
-  [-gb, --bucket]             The name of the bucket to upload the packs to. Default is marketplace-dist-dev.
+  [-ct, --ci-token]           The ci gitlab trigger token.
+  [-b, --branch]              The content repo branch name. Default is the current branch name.
+  [-ib, --infra-branch]       The infra repo branch name. Default is the current branch name.
+  [-ch, --slack-channel]      A slack channel to send notifications to. Default is dmst-bucket-upload.
+  [-gb, --bucket]             The name of the bucket to upload the xsoar marketplace packs to. Default is marketplace-dist-dev.
   [-gb2, --bucket_v2]         The name of the bucket to upload the marketplace v2 packs to. Default is marketplace-v2-dist-dev.
   [-gb3, --bucket_xpanse]     The name of the bucket to upload the xpanse marketplace packs to. Default is xpanse-dist-dev.
   [-gb4, --bucket_xsoar_saas] The name of th bucket to upload the xsoar_saas marketplace packs to. Default is marketplace-saas-dist-dev.
   [-f, --force]               Whether to trigger the force upload flow.
   [-p, --packs]               CSV list of pack IDs. Mandatory when the --force flag is on.
-  [-ch, --slack-channel]      A slack channel to send notifications to. Default is dmst-bucket-upload.
   [-oa, --override-all]       If given, will override all packs during this upload flow.
-  [-ib, --infra-branch]       The infra branch to use. Default is the latest nightly.
   "
+  echo "Get the trigger token from here https://vault.paloaltonetworks.local/home#R2VuZXJpY1NlY3JldERldGFpbHM6RGF0YVZhdWx0OmIyMzJiNDU0LWEzOWMtNGY5YS1hMTY1LTQ4YjRlYzM1OTUxMzpSZWNvcmRJbmRleDowOklzVHJ1bmNhdGVk" # disable-secrets-detection
   exit 1
 fi
 
