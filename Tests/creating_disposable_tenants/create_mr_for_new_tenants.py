f33b87f684b4589f2ab3fc96ec32bb74ca1a29de
Menachem Weinfeld (Contractor) <mwienfeld@paloaltonetworks.com>
2025-02-19 14:28:35 +0000

    New scripts: create_mr_for_new_tenants and wait_for_new_tenants_mr

---
+++ Tests/creating_disposable_tenants/create_mr_for_new_tenants.py
@@ -0,0 +1,214 @@
+import argparse
+import json
+import os
+import sys
+import time
+import uuid
+from collections import OrderedDict
+from distutils.util import strtobool
+from pathlib import Path
+
+import gitlab
+import urllib3
+from gitlab.v4.objects import Project
+from tabulate import tabulate
+
+from Tests.scripts.infra.resources.constants import COMMENT_FIELD_NAME
+from Tests.scripts.utils import logging_wrapper as logging
+from Tests.scripts.utils.log_util import install_logging
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# the default is the id of content-test-conf repo in xdr.pan.local
+GITLAB_PROJECT_ID = str(os.getenv("CI_PROJECT_ID", 1709))
+# disable-secrets-detection
+GITLAB_SERVER_URL = os.getenv("CI_SERVER_URL", "https://gitlab.xdr.pan.local")
+ARTIFACTS_FOLDER: Path = Path(os.getenv("ARTIFACTS_FOLDER", "."))
+MR_NUMBER_FILE: Path = ARTIFACTS_FOLDER / "MR_NUMBER.txt"
+GITLAB_SSL_VERIFY = bool(strtobool(os.getenv("GITLAB_SSL_VERIFY", "true")))
+SLEEP_INTERVAL = 5  # seconds
+ATTEMPTS_NUMBER = 4
+
+
+def get_mr_content(new_tenants_info: list[dict]) -> tuple[str, str]:
+    """Get the title and description for a merge request.
+
+    This function generates a merge request title and description based on the
+    provided new tenants information.
+
+    Args:
+        new_tenants_info (list[dict]): A list of dictionaries containing new tenants' information.
+
+    Returns:
+        tuple[str, str]: A tuple containing:
+            - The generated merge request title.
+            - The generated merge request description.
+    """
+    server_type = set(tenant["server_type"] for tenant in new_tenants_info)
+    flow_type = set(tenant["flow_type"] for tenant in new_tenants_info if tenant["flow_type"])
+    mr_title = (
+        f"Update saas_servers.json with {len(new_tenants_info)} new tenants "
+        f"of server_type {', '.join(server_type)} and flow_type {', '.join(flow_type)}"
+    )
+    mr_description = (
+        "## Update [saas_servers.json](config/saas_servers.json) with the "
+        f"following new tenants:\n{tabulate(new_tenants_info, headers='keys', tablefmt='pipe')}"
+    )
+    return mr_title, mr_description
+
+
+def update_saas_servers_with_new(saas_servers_path: Path, new_tenants_path: Path) -> tuple[dict, list[dict]]:
+    """Updates the SaaS servers information with new tenants' data.
+
+    This function reads SaaS servers and new tenants information from JSON files,
+    merges them, and sorts the result based on server_type and flow_type.
+
+    Args:
+        saas_servers_path (Path): Path to the JSON file containing SaaS servers information.
+        new_tenants_path (Path): Path to the JSON file containing new tenants information.
+
+    Returns:
+        tuple[dict, list]: A tuple containing:
+            - An OrderedDict with the merged and sorted server information.
+            - A list of dictionaries containing the new tenants' information.
+    """
+    saas_servers_info = json.loads(saas_servers_path.read_text())
+    new_tenants_info = json.loads(new_tenants_path.read_text())
+    merged_info: dict = saas_servers_info | new_tenants_info
+
+    sorted_output_data = OrderedDict(
+        sorted(
+            merged_info.items(),
+            key=lambda item: (item[0] != COMMENT_FIELD_NAME, item[1]["server_type"], item[1]["flow_type"]),
+        )
+    )
+    new_tenants = [
+        {"tenant_id": tenant_id, "server_type": tenant_details["server_type"], "flow_type": tenant_details["flow_type"]}
+        for tenant_id, tenant_details in new_tenants_info.items()
+    ]
+    return sorted_output_data, new_tenants
+
+
+def is_merge_request_ready(project: Project, mr_id: int) -> bool:
+    """Check if the merge request is ready to be merged.
+
+    Args:
+        project (Project): The GitLab project object.
+        mr_id (int): The ID of the merge request.
+
+    Returns:
+        bool: True if the merge request is ready to be merged, False otherwise.
+    """
+    logging.info("Waiting for the merge request to be ready for merge.")
+    for i in range(1, ATTEMPTS_NUMBER + 1):
+        logging.debug(f"Checking merge status for attempt {i}/{ATTEMPTS_NUMBER}.")
+        mr = project.mergerequests.get(mr_id)
+        merge_status = mr.merge_status
+        if merge_status != "checking":
+            break
+        logging.debug(f"Current merge status: {merge_status}, sleeping for {SLEEP_INTERVAL} seconds...")
+        time.sleep(SLEEP_INTERVAL)
+
+    logging.info(f"Final merge status: {mr.merge_status}")
+    return mr.merge_status == "can_be_merged"
+
+
+def get_gitlab_project(ci_token: str, project_id: str) -> Project:
+    """Connects to Gitlab and returns the relevant project information.
+    Args:
+        ci_token (int): The ci token to connect to gitlab.
+        project_id (str): The id of the relevant Gitlab project.
+    Returns:
+        A Gitlab project.
+    """
+    gitlab_client = gitlab.Gitlab(GITLAB_SERVER_URL, private_token=ci_token, ssl_verify=GITLAB_SSL_VERIFY)
+    return gitlab_client.projects.get(project_id)
+
+
+def options_handler() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Create MRs for new tenants")
+    parser.add_argument("-c", "--ci-token", help="The token for circleci/gitlab", required=True)
+    parser.add_argument("-r", "--reviewer", help="The gitlab username of the reviewer", required=True)
+    parser.add_argument(
+        "-gp",
+        "--gitlab_project_id",
+        help="The gitlab project id",
+        default=GITLAB_PROJECT_ID,
+        type=str,
+    )
+    parser.add_argument(
+        "--tenants-file-path",
+        required=True,
+        type=Path,
+        help="The path to the file containing the tenants info.",
+    )
+    parser.add_argument(
+        "--cloud-servers-path",
+        required=True,
+        type=Path,
+        help="The path to the file containing the cloud servers info.",
+    )
+    options = parser.parse_args()
+
+    return options
+
+
+def main():
+    install_logging("create_mr_for_new_tenants.log", logger=logging)
+    try:
+        args = options_handler()
+        ci_token = args.ci_token
+        updated_saas_servers, new_tenants_info = update_saas_servers_with_new(args.cloud_servers_path, args.tenants_file_path)
+        mr_title, mr_description = get_mr_content(new_tenants_info)
+
+        logging.info("Connecting to gitlab.")
+        project = get_gitlab_project(ci_token, args.gitlab_project_id)
+
+        branch_name = f"update_saas_servers_with_new_tenants_{uuid.uuid4().hex[:8]}"
+        branch = project.branches.create({"branch": branch_name, "ref": "master"})
+        logging.info(f"Created the branch {branch.name}")
+        logging.debug(f"All branch details:\n{branch.to_json()}")
+
+        commit_data = {
+            "branch": branch_name,
+            "commit_message": mr_title,
+            "actions": [
+                {
+                    "action": "update",
+                    "file_path": "config/saas_servers.json",
+                    "content": json.dumps(updated_saas_servers, indent=2),
+                },
+            ],
+        }
+        commit = project.commits.create(commit_data)
+        logging.info(f"Created the commit {commit.short_id}")
+        logging.debug(f"All commit details:\n{commit.to_json()}")
+        mr = project.mergerequests.create(
+            {
+                "source_branch": branch_name,
+                "target_branch": "master",
+                "title": mr_title,
+                "description": mr_description,
+                "reviewer_ids": [args.reviewer],
+                "remove_source_branch": True,
+            }
+        )
+        mr_id = mr.get_id()
+        logging.info(f"Merge request created with ID {mr_id}, url: {mr.web_url}")
+        logging.debug(f"All merge request details:\n{mr.to_json()}")
+        if is_merge_request_ready(project, mr_id):
+            logging.info("Merge request is ready to be merged.")
+            mr.merge(merge_when_pipeline_succeeds=True)
+        else:
+            logging.warning("Merge request is not ready to be merged, it should be manually merged.")
+
+        # Write the merge request number to a file
+        MR_NUMBER_FILE.write_text(str(mr.iid))
+        logging.info(f"Merge request number written to {MR_NUMBER_FILE.as_posix()}")
+    except Exception as e:
+        logging.exception(f"Error occurred: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Tests/creating_disposable_tenants/wait_for_new_tenants_mr.py
@@ -0,0 +1,70 @@
+import argparse
+import sys
+import time
+
+import urllib3
+
+from Tests.creating_disposable_tenants.create_mr_for_new_tenants import GITLAB_PROJECT_ID, MR_NUMBER_FILE, get_gitlab_project
+from Tests.scripts.utils import logging_wrapper as logging
+from Tests.scripts.utils.log_util import install_logging
+
+# Disable insecure warnings
+urllib3.disable_warnings()
+
+
+SLEEP_TIMEOUT = 60 * 5  # 5 minutes
+TIMEOUT = 60 * 60 * 6  # 6 hours
+
+
+def options_handler() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Creates release pull request for demisto-sdk.")
+    parser.add_argument("-c", "--ci-token", help="The token for gitlab", required=True)
+    parser.add_argument("-mr", "--mr-number", help="The MR number to wait for", required=False)
+
+    return parser.parse_args()
+
+
+def main():
+    install_logging("wait_for_new_tenants_mr.log", logger=logging)
+    try:
+        args = options_handler()
+        ci_token = args.ci_token
+        mr_number = int(args.mr_number or MR_NUMBER_FILE.read_text().strip())
+
+        project = get_gitlab_project(ci_token, GITLAB_PROJECT_ID)
+        # initialize timer
+        start = time.time()
+        elapsed: float = 0
+
+        logging.info(f"Started waiting for MR #{mr_number} to be merged")
+        # wait to mr to be closed
+        while elapsed < TIMEOUT:
+            mr = project.mergerequests.get(mr_number)
+
+            mr_state = mr.state
+            logging.info(f"The current state of the MR is: {mr_state}")
+
+            if mr_state != "opened":
+                break
+
+            elapsed = time.time() - start
+
+            if elapsed >= TIMEOUT:
+                logging.error("Timeout reached while waiting for SDK and content pull requests to be merged")
+                sys.exit(1)
+
+            logging.info(f"Go to sleep for {SLEEP_TIMEOUT//60} minutes, and check again.")
+            time.sleep(SLEEP_TIMEOUT)
+
+        # check that the mr is merged
+        if mr.state != "merged":
+            raise Exception(f"The merge request #{mr_number} was closed but not merged. url: {mr.web_url}")
+
+        logging.success(f"The merge request #{mr_number} has been merged successfully!")
+    except Exception as e:
+        logging.exception(e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
