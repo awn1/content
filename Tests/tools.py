2124f4571536ec40ffbdb2227cd542810cefecf8
Moshe Eichler <meichler@paloaltonetworks.com>
2025-03-03 10:40:11 +0000

    Remove MITMProxy server from server build

--- Tests/tools.py
+++ Tests/tools.py
@@ -1,39 +1,8 @@
 import argparse
-from collections.abc import Callable
-from functools import wraps
 
 import requests
-from demisto_sdk.commands.test_content.mock_server import MITMProxy
 
 from SecretActions.google_secret_manager_handler import get_secrets_from_gsm
-
-
-def run_with_proxy_configured(function: Callable) -> Callable:
-    """
-    This is a decorator for the 'instance_testing method`.
-    This decorator configures the proxy in the server before the instance_testing execution and removes it afterwards.
-    Args:
-        function: Should be the instance_testing method.
-    """
-
-    @wraps(function)
-    def decorated(server, *args, **kwargs):
-        server.build.proxy.configure_proxy_in_demisto(
-            proxy=server.build.servers[0].internal_ip + ":" + MITMProxy.PROXY_PORT,
-            username=server.user_name,
-            password=server.password,
-            server=f"https://{server.build.servers[0].internal_ip}",
-        )
-        result = function(server, *args, **kwargs)
-        server.build.proxy.configure_proxy_in_demisto(
-            proxy="",
-            username=server.user_name,
-            password=server.password,
-            server=f"https://{server.build.servers[0].internal_ip}",
-        )
-        return result
-
-    return decorated
 
 
 def get_integration_params(options: argparse.Namespace, instance_name: str) -> dict:
--- server-image-build/resources/install_proxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-python3 -m pip install --user pipx
-python3 -m pipx ensurepath
-. ~/.profile
-pipx install mitmproxy
-pipx inject mitmproxy dateparser MarkupSafe==2.0.1
