c7fc387cbd867018ce50e6aab8d239027234a39c
Roei Shunim <rshunim@paloaltonetworks.com>
2025-04-17 12:28:39 +0000

    adding logs

--- Utils/start_stop_mechanism_test.py
+++ Utils/start_stop_mechanism_test.py
@@ -1,19 +1,33 @@
 import logging
-import os
 from unittest.mock import patch
 
 import pytest
 
+from Tests.scripts.infra.viso_api import VisoAPI
+
+
+@pytest.fixture(autouse=True)
+def set_env_vars(monkeypatch):
+    monkeypatch.setenv("VISO_API_URL", "http://example.com")
+    monkeypatch.setenv("VISO_API_KEY", "123")
+    monkeypatch.setenv("GROUP_OWNER", "group_owner")
+
 
 @pytest.fixture
 def mocked_viso():
-    from Tests.scripts.infra.viso_api import VisoAPI
-
     return VisoAPI(base_url="http://example.com", api_key="123")
 
 
-@patch.dict(os.environ, {"VISO_API_URL": "http://example.com", "VISO_API_KEY": "123", "GROUP_OWNER": "group_owner"})
-def test_start_stop_mechanism_success(mocker, mocked_viso, requests_mock):
+@pytest.fixture
+def mock_tenants():
+    return [
+        {"lcaas_id": "tenant1", "status": "running", "stop_status": "stopped"},
+        {"lcaas_id": "tenant2", "status": "running", "stop_status": "started"},
+    ]
+
+
+@patch.object(VisoAPI, "get_all_tenants")
+def test_start_stop_mechanism_success(mock_get_all_tenants, mocked_viso, mock_tenants, mocker):
     """
     Given:
         - Two tenants which are running, one its stop_status is 'stopped' and the second is 'started'
@@ -26,27 +40,23 @@ def test_start_stop_mechanism_success(mocker, mocked_viso, requests_mock):
          2. tenant2 is success too, since it was sent to execution stop action, and we mocked its result as success)
     """
 
-    from start_stop_mechanism import TenantReport
+    from Utils.start_stop_mechanism import TenantReport
 
-    all_tenants = [
-        {"lcaas_id": "tenant1", "status": "running", "stop_status": "stopped"},
-        {"lcaas_id": "tenant2", "status": "running", "stop_status": "started"},
-    ]
-    requests_mock.get("http://example.com/api/v4.0/tenants/", json=all_tenants)
-    mocker.patch.object(mocked_viso, "get_all_tenants")
+    mock_get_all_tenants.return_value = mock_tenants
+
     report_1 = [TenantReport(lcaas_id="tenant1", status="running", stop_status="stopped", success=True)]
     report_2 = [TenantReport(lcaas_id="tenant2", status="running", stop_status="stopped", success=True)]
-    mocker.patch("start_stop_mechanism.start_stop_machines", return_value=report_2)
+    mocker.patch("Utils.start_stop_mechanism.start_stop_machines", return_value=report_2)
 
-    from start_stop_mechanism import start_stop_mechanism
+    from Utils.start_stop_mechanism import start_stop_mechanism
 
     result = start_stop_mechanism(tenants_id={"tenant1", "tenant2"}, action="stop")
 
     assert result == report_2 + report_1
 
 
-@patch.dict(os.environ, {"VISO_API_URL": "http://example.com", "VISO_API_KEY": "123", "GROUP_OWNER": "group_owner"})
-def test_start_stop_mechanism_no_tenants(mocker, mocked_viso, requests_mock, caplog):
+@patch.object(VisoAPI, "get_all_tenants")
+def test_start_stop_mechanism_no_tenants(mock_get_all_tenants, mocked_viso, caplog):
     """
     Given:
         - no tenants
@@ -56,9 +66,9 @@ def test_start_stop_mechanism_no_tenants(mocker, mocked_viso, requests_mock, cap
         - Ensure result is an empty list
         - Ensure the logs as expected and informative (since no tenants are retrieved)
     """
-    requests_mock.get("http://example.com/api/v4.0/tenants/", json=[])
-    mocker.patch.object(mocked_viso, "get_all_tenants")
-    from start_stop_mechanism import start_stop_mechanism
+    mock_get_all_tenants.return_value = []
+
+    from Utils.start_stop_mechanism import start_stop_mechanism
 
     with caplog.at_level(logging.INFO):
         result = start_stop_mechanism(tenants_id={"tenant1"}, action="stop")
@@ -68,8 +78,8 @@ def test_start_stop_mechanism_no_tenants(mocker, mocked_viso, requests_mock, cap
     assert "No relevant tenants were found to execute the action='stop' on." in logs[0]
 
 
-@patch.dict(os.environ, {"VISO_API_URL": "http://example.com", "VISO_API_KEY": "123", "GROUP_OWNER": "group_owner"})
-def test_start_stop_mechanism_sanity_check(mocker, mocked_viso, requests_mock, caplog):
+@patch.object(VisoAPI, "get_all_tenants")
+def test_start_stop_mechanism_sanity_check(mock_get_all_tenants, mocked_viso, caplog):
     """
     Given:
         - A single tenant which is running and its stop_status is stopped
@@ -79,13 +89,10 @@ def test_start_stop_mechanism_sanity_check(mocker, mocked_viso, requests_mock, c
         - Ensure result contains one report for this tenant with success=True
         - Ensure the logs as expected and informative (since the tenant is already ready an no action is needed)
     """
-    requests_mock.get(
-        "http://example.com/api/v4.0/tenants/", json=[{"lcaas_id": "tenant1", "status": "running", "stop_status": "stopped"}]
-    )
-    mocker.patch.object(mocked_viso, "get_all_tenants")
+    mock_get_all_tenants.return_value = [{"lcaas_id": "tenant1", "status": "running", "stop_status": "stopped"}]
 
     # Now call the function
-    from start_stop_mechanism import TenantReport, start_stop_mechanism
+    from Utils.start_stop_mechanism import TenantReport, start_stop_mechanism
 
     with caplog.at_level(logging.INFO):
         result = start_stop_mechanism(tenants_id={"tenant1"}, action="stop")
