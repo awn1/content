e8d800c521af5b35cc7132c4d64ad5e457ba0ece
iapt@paloaltonetworks.com <iapt@paloaltonetworks.com>
2025-09-07 10:03:51 +0300

    pre-commit fix

--- Packs/Base/Scripts/SendNotificationAgentix/SendNotificationAgentix.py
+++ Packs/Base/Scripts/SendNotificationAgentix/SendNotificationAgentix.py
@@ -6,7 +6,8 @@ from CommonServerUserPython import *
 def send_notification_by_brand(brand: str, args: dict):
     args.update({"using-brand": brand})
     return demisto.executeCommand("send-notification", args=args)
-        
+
+
 def main():
     try:
         args = demisto.args()
